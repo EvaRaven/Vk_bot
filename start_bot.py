a263a36961034817150d69211024cd146f23496c
EvaRaven <46091924+EvaRaven@users.noreply.github.com>
2018-12-23 23:18:38 +0400

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -4,7 +4,7 @@ from core import bot_vk
 
 
 def main():
-    bot_vk('4b1f83834f889d023ecd1b827d21b7211ba85aa391a3aa465d91724b972aa7b870e26d20428c61a6a9994')
+    bot_vk('')
 
 
 if __name__ == '__main__':
